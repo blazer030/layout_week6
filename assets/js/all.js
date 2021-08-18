4e21e8c5b6a7b5c7dc9dd15211cef399cd7231b6
ethan <blazer030@gmail.com>
2021-08-19 04:32:08 +0800

    Update 2021-08-18T20:31:51.865Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,9 +2,32 @@
 
 $(document).ready(function () {
   console.log("i'm ready");
-  $('.recommend-list').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    breakpoints: {
+      // when window width is >= 480px
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 640px
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      1400: {
+        slidesPerView: 3,
+        spaceBetween: '120%'
+      }
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
   });
 });
 //# sourceMappingURL=all.js.map
