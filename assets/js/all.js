c4105412583e84c31b3c730322131c47544887ee
ethan <blazer030@gmail.com>
2021-08-18 22:50:50 +0800

    Update 2021-08-18T14:50:26.194Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,5 +2,9 @@
 
 $(document).ready(function () {
   console.log("i'm ready");
+  $('.recommend-list').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
 });
 //# sourceMappingURL=all.js.map
