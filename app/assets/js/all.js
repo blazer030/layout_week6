cd684857823e0b0ff24ad7c5ec012dd68882a70c
ethan <blazer030@gmail.com>
2021-08-19 01:07:05 +0800

    [edit]調整圖片大小、調整推薦區塊效果

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,30 @@
 $(document).ready(() => {
     console.log("i'm ready");
+    var swiper = new Swiper(".swiper-container", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      centeredSlides: true,
+      loop: true,
+      breakpoints: {
+        // when window width is >= 480px
+        576: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        // when window width is >= 640px
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        // when window width is >= 640px
+        1400: {
+          slidesPerView: 3,
+          spaceBetween: '120%',
+        }
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
 });
